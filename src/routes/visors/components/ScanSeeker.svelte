09f58cba72501f2db77046c80501ede78e2e6548
Tyler Johnson <TylerTonyJohnson@gmail.com>
2023-07-04 21:36:37 -0700

    Moooore

--- src/routes/visors/components/ScanSeeker.svelte
+++ src/routes/visors/components/ScanSeeker.svelte
@@ -1,21 +1,22 @@
 <script>
-	import { fade } from 'svelte/transition';
-	import { isLockable, isLocked, seekerPositions } from '../../../lib/stores';
+	import { isLockable, isLocked, closestSeekerPosition } from '../../../lib/stores';
+	let x = 50;
+	let y = 50;
 </script>
 
-{#if $isLockable}
-	{#each $seekerPositions as seekerPosition}
-		{@const x = (100 * (seekerPosition.x + 1)) / 2}
-		{@const y = 100 * (1 - (seekerPosition.y + 1) / 2)}
-		{#if true}
-			<img
-				id="cursor"
-				src="Scan Seeker Yellow 1x.png"
-				style="left: {x}%; top: {y}%;"
-				alt="Combat Seeker"
-			/>
-		{/if}
-	{/each}
+{#if isLockable}
+	{@const x = (100 * ($closestSeekerPosition.x + 1)) / 2}
+	{@const y = 100 * (1 - ($closestSeekerPosition.y + 1) / 2)}
+	{@const dist = Math.sqrt(Math.pow(50 - x, 2) + Math.pow(50 - y, 2))}
+	{#if dist < 30}
+		<img
+			id="cursor"
+			src="Scan Seeker 1x.png"
+			style="left: {x}%;
+            top: {y}%;"
+			alt="Combat Seeker"
+		/>
+	{/if}
 {/if}
 
 <style>
@@ -24,6 +25,6 @@
 		left: 50%;
 		top: 50%;
 		translate: -50% -50%;
-		width: 30px;
+		transition: rotate 2s ease-out;
 	}
 </style>
