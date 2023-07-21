e5be3a2d8ee35372a40e1ace88656b9fc553e273
Tyler Johnson <TylerTonyJohnson@gmail.com>
2023-07-21 12:04:10 -0700

    Cleanup

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,19 @@
 import adapter from '@sveltejs/adapter-static';
-import {
-    vitePreprocess
-} from '@sveltejs/kit/vite';
- 
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: vitePreprocess(),
- 
-    kit: {
-        adapter: adapter({
-			pages: "docs",
-			assets: "docs"
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs'
 		}),
-        paths: {
-            base: process.env.NODE_ENV === 'production' ? '/Metroid' : '',
-        }
-    }
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/Metroid' : ''
+		}
+	}
 };
- 
+
 export default config;
