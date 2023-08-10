67b77d95664ea7999db11ef6590a8fdb3bb7e853
Tyler Johnson <TylerTonyJohnson@gmail.com>
2023-08-09 22:07:21 -0700

    Basic physics refactored

--- src/lib/experience/utils/Debug.js
+++ src/lib/experience/utils/Debug.js
@@ -1,13 +1,21 @@
+import CannonDebugger from 'cannon-es-debugger';
 import * as GUI from 'lil-gui';
+import Stats from 'three/examples/jsm/libs/stats.module';
 
 export default class Debug {
     constructor() {
-        this.active = window.location.hash === '#debug';
-        // this.active = true;
-        
-        if(this.active) {
-            this.gui = new GUI.GUI();
-        }
+        this.isActive = window.location.hash === '#debug';
 
+        // Setup 
+        if(!this.isActive) return; 
+            this.gui = new GUI.GUI({ closeFolders: true });
+            this.stats = new Stats();
+            this.cannonDebugger = null;
+            document.body.appendChild(this.stats.dom);
+    }
+
+    update() {
+        if (this.stats) this.stats.update();
+        if (this.cannonDebugger) this.cannonDebugger.update();
     }
 }
