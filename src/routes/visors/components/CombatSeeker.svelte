7a2424460f7233f86e3b65c73be28458960dc7e9
Tyler Johnson <TylerTonyJohnson@gmail.com>
2023-09-03 17:48:28 -0700

    Itemz

--- src/routes/visors/components/CombatSeeker.svelte
+++ src/routes/visors/components/CombatSeeker.svelte
@@ -1,68 +1,50 @@
 <script>
-	import { appState, isRendering, isLockable, isLocked, closestSeekerPosition } from '../../../lib/stores';
+	import { appState, isLockable, isLocked, closestSeekerPosition } from '../../../lib/stores';
 	import { AppState } from '../../../lib/enums';
-	import { mapRange } from '../../../lib/math';
-	import { fade } from 'svelte/transition';
 	import { tweened } from 'svelte/motion';
-
-
-	$: aspect = window.innerWidth / window.innerHeight;
-	$: matchingWidth = 100 / aspect;
-	$: matchMin = (100 - matchingWidth) / 2;
-	$: matchMax = matchingWidth + matchMin;
 
 	$: x = (100 * ($closestSeekerPosition.x + 1)) / 2;
 	$: y = 100 * (1 - ($closestSeekerPosition.y + 1) / 2);
-	
-	const opacity = tweened(0, { duration: 100 });
-	$: { opacity.set($isLockable ? 1 : 0) };
 
-	// $: {
-	// 	switch (true) {
-	// 		case x < matchMin:
-	// 			opacity = mapRange(x, 0, matchMin, 0, 1);
-	// 			break;
-	// 		case x > matchMax:
-	// 			opacity = mapRange(x, matchMax, 100, 1, 0);
-	// 	}
-	// }
+	const opacity = tweened(0, { duration: 100 });
+	$: {
+		opacity.set(($isLockable ? 1 : 0) * (1 - Math.pow($closestSeekerPosition.x, 2)));
+	}
 </script>
 
-<!-- {#if $isLockable} -->
-	<svg
-		class="cursor rotate"
-		style="left: {x}%;
+<svg
+	class="cursor rotate"
+	style="left: {x}%;
             top: {y}%;
 			opacity: {$opacity};"
-		class:locked={$isLocked}
-		class:paused={$appState === AppState.Paused}
-		viewBox="0 0 540 540"
-		fill="none"
-		xmlns="http://www.w3.org/2000/svg"
-	>
-		<!-- Arrows -->
-		<path class="arrow" d="M267 108H270H273L275 105H280L270 122L260 105H265L267 108Z" />
-		<path
-			class="arrow"
-			d="M131.204 353.598L129.704 351L128.204 348.402L124.606 348.17L122.106 343.84L141.828 344L132.106 361.16L129.606 356.83L131.204 353.598Z"
-		/>
-		<path
-			class="arrow"
-			d="M411.811 348.406L410.311 351.004L408.811 353.602L410.409 356.834L407.909 361.164L398.186 344.004L417.909 343.844L415.409 348.174L411.811 348.406Z"
-		/>
+	class:locked={$isLocked}
+	class:paused={$appState === AppState.Paused}
+	viewBox="0 0 540 540"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<!-- Arrows -->
+	<path class="arrow" d="M267 108H270H273L275 105H280L270 122L260 105H265L267 108Z" />
+	<path
+		class="arrow"
+		d="M131.204 353.598L129.704 351L128.204 348.402L124.606 348.17L122.106 343.84L141.828 344L132.106 361.16L129.606 356.83L131.204 353.598Z"
+	/>
+	<path
+		class="arrow"
+		d="M411.811 348.406L410.311 351.004L408.811 353.602L410.409 356.834L407.909 361.164L398.186 344.004L417.909 343.844L415.409 348.174L411.811 348.406Z"
+	/>
 
-		<!-- Boxes -->
-		<path class="box" d="M263 102V95H277V102H273L271 105H270H269L267 102H263Z" />
-		<path
-			class="box"
-			d="M128.008 360.062L121.946 363.562L114.946 351.438L121.008 347.938L123.008 351.402L126.606 351.634L127.106 352.5L127.606 353.366L126.008 356.598L128.008 360.062Z"
-		/>
-		<path
-			class="box"
-			d="M419.007 347.942L425.069 351.442L418.069 363.566L412.007 360.066L414.007 356.602L412.409 353.37L412.909 352.504L413.409 351.638L417.007 351.406L419.007 347.942Z"
-		/>
-	</svg>
-<!-- {/if} -->
+	<!-- Boxes -->
+	<path class="box" d="M263 102V95H277V102H273L271 105H270H269L267 102H263Z" />
+	<path
+		class="box"
+		d="M128.008 360.062L121.946 363.562L114.946 351.438L121.008 347.938L123.008 351.402L126.606 351.634L127.106 352.5L127.606 353.366L126.008 356.598L128.008 360.062Z"
+	/>
+	<path
+		class="box"
+		d="M419.007 347.942L425.069 351.442L418.069 363.566L412.007 360.066L414.007 356.602L412.409 353.37L412.909 352.504L413.409 351.638L417.007 351.406L419.007 347.942Z"
+	/>
+</svg>
 
 <style>
 	.cursor {
